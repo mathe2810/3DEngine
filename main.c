05ae9128a63ef48cbba432d359f51919b07df3d9
mathe2810 <matheogleon92100@yahoo.com>
2023-09-17 19:35:36 +0200

    changes

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 
 typedef struct
 {
-    float x,y,z;
+    float x,y,z,w;
 }vec3d;
 
 typedef struct
@@ -14,6 +14,11 @@ typedef struct
     int color;
     float ApproxZ;
 }t_triangle;
+
+typedef struct
+{
+    float m[4][4] ;
+}mat4x4;
 
 void init()
 {
@@ -65,6 +70,192 @@ void MultiplyMatrixVector(vec3d *i, vec3d *o, float m[4][4])
     {
         o->x /= w; o->y /= w; o->z /= w;
     }
+}
+
+mat4x4 Matrix_MakeIdentity()
+{
+    mat4x4 matrix;
+    matrix.m[0][0] = 1.0f;
+    matrix.m[1][1] = 1.0f;
+    matrix.m[2][2] = 1.0f;
+    matrix.m[3][3] = 1.0f;
+    return matrix;
+}
+
+mat4x4 Matrix_MakeRotationX(float fAngleRad)
+{
+    mat4x4 matrix;
+    matrix.m[0][0] = 1.0f;
+    matrix.m[1][1] = cosf(fAngleRad);
+    matrix.m[1][2] = sinf(fAngleRad);
+    matrix.m[2][1] = -sinf(fAngleRad);
+    matrix.m[2][2] = cosf(fAngleRad);
+    matrix.m[3][3] = 1.0f;
+    return matrix;
+}
+
+mat4x4 Matrix_MakeRotationY(float fAngleRad)
+{
+    mat4x4 matrix;
+    matrix.m[0][0] = cosf(fAngleRad);
+    matrix.m[0][2] = sinf(fAngleRad);
+    matrix.m[2][0] = -sinf(fAngleRad);
+    matrix.m[1][1] = 1.0f;
+    matrix.m[2][2] = cosf(fAngleRad);
+    matrix.m[3][3] = 1.0f;
+    return matrix;
+}
+
+mat4x4 Matrix_MakeRotationZ(float fAngleRad)
+{
+    mat4x4 matrix;
+    matrix.m[0][0] = cosf(fAngleRad);
+    matrix.m[0][1] = sinf(fAngleRad);
+    matrix.m[1][0] = -sinf(fAngleRad);
+    matrix.m[1][1] = cosf(fAngleRad);
+    matrix.m[2][2] = 1.0f;
+    matrix.m[3][3] = 1.0f;
+    return matrix;
+}
+
+mat4x4 Matrix_MakeTranslation(float x, float y, float z)
+{
+    mat4x4 matrix;
+    matrix.m[0][0] = 1.0f;
+    matrix.m[1][1] = 1.0f;
+    matrix.m[2][2] = 1.0f;
+    matrix.m[3][3] = 1.0f;
+    matrix.m[3][0] = x;
+    matrix.m[3][1] = y;
+    matrix.m[3][2] = z;
+    return matrix;
+}
+
+mat4x4 Matrix_MakeProjection(float fFovDegrees, float fAspectRatio, float fNear, float fFar)
+{
+    float fFovRad = 1.0f / tanf(fFovDegrees * 0.5f / 180.0f * 3.14159f);
+    mat4x4 matrix;
+    matrix.m[0][0] = fAspectRatio * fFovRad;
+    matrix.m[1][1] = fFovRad;
+    matrix.m[2][2] = fFar / (fFar - fNear);
+    matrix.m[3][2] = (-fFar * fNear) / (fFar - fNear);
+    matrix.m[2][3] = 1.0f;
+    matrix.m[3][3] = 0.0f;
+    return matrix;
+}
+
+mat4x4 Matrix_MultiplyMatrix(mat4x4 *m1, mat4x4 *m2)
+{
+mat4x4 matrix;
+    for (int c = 0; c < 4; c++)
+    {
+        for (int r = 0; r < 4; r++){
+            matrix.m[r][c] = m1->m[r][0] * m2->m[0][c] + m1->m[r][1] * m2->m[1][c] + m1->m[r][2] * m2->m[2][c] + m1->m[r][3] * m2->m[3][c];
+        }
+    }
+return matrix;
+}
+
+/*mat4x4 Matrix_PointAt(vec3d *pos, vec3d *target, vec3d *up)
+{
+// Calculate new forward direction
+vec3d newForward = Vector_Sub(target, pos);
+newForward = Vector_Normalise(newForward);
+
+// Calculate new Up direction
+vec3d a = Vector_Mul(newForward, Vector_DotProduct(up, newForward));
+vec3d newUp = Vector_Sub(up, a);
+newUp = Vector_Normalise(newUp);
+
+// New Right direction is easy, its just cross product
+vec3d newRight = Vector_CrossProduct(newUp, newForward);
+
+// Construct Dimensioning and Translation Matrix
+mat4x4 matrix;
+matrix.m[0][0] = newRight.x;	matrix.m[0][1] = newRight.y;	matrix.m[0][2] = newRight.z;	matrix.m[0][3] = 0.0f;
+matrix.m[1][0] = newUp.x;		matrix.m[1][1] = newUp.y;		matrix.m[1][2] = newUp.z;		matrix.m[1][3] = 0.0f;
+matrix.m[2][0] = newForward.x;	matrix.m[2][1] = newForward.y;	matrix.m[2][2] = newForward.z;	matrix.m[2][3] = 0.0f;
+matrix.m[3][0] = pos.x;			matrix.m[3][1] = pos.y;			matrix.m[3][2] = pos.z;			matrix.m[3][3] = 1.0f;
+return matrix;
+
+}*/
+
+mat4x4 Matrix_QuickInverse(mat4x4 *m) // Only for Rotation/Translation Matrices
+{
+mat4x4 matrix;
+matrix.m[0][0] = m->m[0][0]; matrix.m[0][1] = m->m[1][0]; matrix.m[0][2] = m->m[2][0]; matrix.m[0][3] = 0.0f;
+matrix.m[1][0] = m->m[0][1]; matrix.m[1][1] = m->m[1][1]; matrix.m[1][2] = m->m[2][1]; matrix.m[1][3] = 0.0f;
+matrix.m[2][0] = m->m[0][2]; matrix.m[2][1] = m->m[1][2]; matrix.m[2][2] = m->m[2][2]; matrix.m[2][3] = 0.0f;
+matrix.m[3][0] = -(m->m[3][0] * matrix.m[0][0] + m->m[3][1] * matrix.m[1][0] + m->m[3][2] * matrix.m[2][0]);
+matrix.m[3][1] = -(m->m[3][0] * matrix.m[0][1] + m->m[3][1] * matrix.m[1][1] + m->m[3][2] * matrix.m[2][1]);
+matrix.m[3][2] = -(m->m[3][0] * matrix.m[0][2] + m->m[3][1] * matrix.m[1][2] + m->m[3][2] * matrix.m[2][2]);
+matrix.m[3][3] = 1.0f;
+return matrix;
+}
+
+vec3d Vector_Add(vec3d *v1, vec3d *v2)
+{
+    vec3d vToReturn;
+    vToReturn.x = v1->x + v2->x;
+    vToReturn.y = v1->y + v2->y;
+    vToReturn.z = v1->z + v2->z;
+return vToReturn;
+}
+
+vec3d Vector_Sub(vec3d *v1, vec3d *v2)
+{
+    vec3d vToReturn;
+    vToReturn.x = v1->x - v2->x;
+    vToReturn.y = v1->y - v2->y;
+    vToReturn.z = v1->z - v2->z;
+return vToReturn;
+}
+
+vec3d Vector_Mul(vec3d *v1, float k)
+{
+    vec3d vToReturn;
+    vToReturn.x = v1->x * k;
+    vToReturn.y = v1->y * k;
+    vToReturn.z = v1->z * k;
+    return vToReturn;
+}
+
+vec3d Vector_Div(vec3d *v1, float k)
+{
+    vec3d vToReturn;
+    vToReturn.x = v1->x / k;
+    vToReturn.y = v1->y / k;
+    vToReturn.z = v1->z / k;
+    return vToReturn;
+}
+
+float Vector_DotProduct(vec3d *v1, vec3d *v2)
+{
+    return v1->x*v2->x + v1->y*v2->y + v1->z * v2->z;
+}
+
+float Vector_Length(vec3d *v)
+{
+    return sqrtf(Vector_DotProduct(v, v));
+}
+
+vec3d Vector_Normalise(vec3d *v)
+{
+    float l = Vector_Length(v);
+    vec3d vToReturn;
+    vToReturn.x = v->x / l;
+    vToReturn.y = v->y / l;
+    vToReturn.z = v->z / l;
+    return vToReturn;
+}
+
+vec3d Vector_CrossProduct(vec3d *v1, vec3d *v2)
+{
+    vec3d v;
+    v.x = v1->y * v2->z - v1->z * v2->y;
+    v.y = v1->z * v2->x - v1->x * v2->z;
+    v.z = v1->x * v2->y - v1->y * v2->x;
+return v;
 }
 
 int GetColour(float lum)
@@ -153,7 +344,7 @@ int main() {
     init();
 
     FILE *pf;
-    pf= fopen("../sphere.obj","r");
+    pf= fopen("../axes.obj","r");
     if(pf==NULL)
     {
         allegro_message("could not open obj");
@@ -250,7 +441,7 @@ int main() {
     t_triangle *triangleToRaster= malloc(sizeof (t_triangle)*nbTriangle);
 
 
-    float fTheta=0,fPhi=0,fAngleRad=0;
+    float fTheta=M_PI,fPhi=0,fAngleRad=0;
     float dx=0,dy=0,dz=100;
     float fNear = 0.1f;
     float fFar = 1000.0f;
@@ -309,16 +500,16 @@ int main() {
 
         if(key [KEY_W])
         {
-            fTheta+=0.1f;
+            fTheta+=0.01f;
         }
         if(key [KEY_E])
         {
-            fPhi+=0.1f;
+            fPhi+=0.01f;
         }
 
         if(key [KEY_R])
         {
-            fAngleRad+=0.1f;
+            fAngleRad+=0.01f;
         }
 
 
